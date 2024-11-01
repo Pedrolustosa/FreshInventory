2dc79a91c18c091edf98f9a83ec03f42323650d4
Pedrolustosa <pedroeternalss@gmail.com>
2024-11-01 16:12:05 -0300

    feat: integrate email notifications for ingredient actions
    - Implemented EmailService using MailKit to send emails via SMTP
    - Created IEmailService interface for abstraction
    - Configured email settings in appsettings.json
    - Updated IngredientService to send emails on create, update, and delete operations
      - GenerateCreateEmailBody: Includes details of the created ingredient
      - GenerateUpdateEmailBody: Shows old and new data of the updated ingredient
      - GenerateDeleteEmailBody: Includes details of the deleted ingredient
    - Ensured emails are sent to ABC@gmail.com
    - Registered EmailService and settings in dependency injection container
    - Added error handling and logging around email sending operations

---
+++ FreshInventory.Application/DTO/EmailSettingsDto.cs
@@ -0,0 +1,3 @@
+﻿namespace FreshInventory.Application.DTO;
+
+public record EmailSettingsDto(string From, string SmtpServer, string UserName, string Password);
---
+++ FreshInventory.Application/Services/EmailService.cs
@@ -0,0 +1,28 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using FreshInventory.Application.DTO;
+using FreshInventory.Application.Interfaces;
+
+namespace FreshInventory.Application.Services;
+
+public class EmailService(EmailSettingsDto emailSettings) : IEmailService
+{
+    private readonly EmailSettingsDto _emailSettings = emailSettings;
+
+    public async Task SendEmailAsync(string subject, string body)
+    {
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress(_emailSettings.From, _emailSettings.From));
+        message.To.Add(new MailboxAddress("", "EMAIL"));
+        message.Subject = subject;
+        message.Body = new TextPart("html")
+        {
+            Text = body
+        };
+        using var client = new SmtpClient();
+        await client.ConnectAsync(_emailSettings.SmtpServer, 587, MailKit.Security.SecureSocketOptions.StartTls);
+        await client.AuthenticateAsync(_emailSettings.UserName, _emailSettings.Password);
+        await client.SendAsync(message);
+        await client.DisconnectAsync(true);
+    }
+}
